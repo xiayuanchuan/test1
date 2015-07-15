ab88faf8aba4de03c238dfa4b6cd1b0d8e3b6936
xiayuanchuan <xiayuanchuan@bravesoft.co.jp>
2015-07-15 16:05:14 +0800

    实现了动画显示抽奖背景 但是文字还存在问题，需要计算 Signed-off-by: xiayuanchuan <xiayuanchuan@bravesoft.co.jp>

--- dazhuanpan/choujiang.php
+++ dazhuanpan/choujiang.php
@@ -13,6 +13,7 @@ class chouJiang {
         $temp = array();
         $weight = 0;
         $data = json_decode(file_get_contents("./data.txt"), true);
+        $dataCount=count($data);
         foreach ($data as $key => $value) {
             $weight += intval($value['weight']);
 
@@ -22,7 +23,7 @@ class chouJiang {
         }
         if ($weight == 0) {
             unlink("./data.txt");
-            return false;
+            return $dataCount;
         }
 
         $int = mt_rand(0, $weight - 1);
@@ -31,7 +32,8 @@ class chouJiang {
             $data[$chouId]['weight'] --;
         }
         file_put_contents("./data.txt", json_encode($data));
-        $temp[$int]['count'] = $weight;
+        $temp[$int]['left'] = $weight;
+        $temp[$int]['count'] = $dataCount;
         return $temp[$int];
     }
 
@@ -40,7 +42,7 @@ class chouJiang {
 if ($_GET['jiangNum'] == 1) {
 
         $choujiangNum = array(
-            0 => array('id' => 0, 'anme' => '一等奖', 'weight' => 10),
+           0 => array('id' => 0, 'anme' => '一等奖', 'weight' => 10),
             1 => array('id' => 1, 'anme' => '七等奖', 'weight' => 15),
             2 => array('id' => 2, 'anme' => '六等奖', 'weight' => 25),
             3 => array('id' => 3, 'anme' => '七等奖', 'weight' => 50),
@@ -77,6 +79,7 @@ if ($_GET['jiangNum'] == 1) {
             9 => array('id' => 9, 'anme' => '七等奖', 'weight' => 150),
             10 => array('id' => 10, 'anme' => '二等奖', 'weight' => 150),
             11 => array('id' => 11, 'anme' => '七等奖', 'weight' => 150),
+        
         );
         file_put_contents($filename, json_encode($data));
     }
@@ -85,16 +88,18 @@ if ($_GET['jiangNum'] == 1) {
     $res = chouJiang::countWeight();
     $data = null;
     $a = rand(1, 29);
-    if (!$res) {
+    if (!$res["anme"]) {
         $data['state'] = 0;
         $data['message'] = "所有奖品都已经抽完啦!";
         $data['data'] = $res;
         $data['arang'] = $a;
+        $data['count']=$res;
     } else {
         $data['state'] = 1;
         $data['message'] = "";
         $data['data'] = $res;
         $data['arang'] = $a;
+        $data['count']=$res['count'];
     }
     echo json_encode($data);
     exit();
